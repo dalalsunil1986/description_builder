eaba096f3975fada6f2deb47e60f74bda03407bd
Mark Tellez <mark.tellez@icloud.com>
2018-05-06 23:01:11 -0500

    added youtube_id

--- app/models/description.rb
+++ app/models/description.rb
@@ -1,6 +1,7 @@
 class Description < ApplicationRecord
   validates :no, :title, :description, :repo_url,
-            :length, :skill_level, :skill_list, presence: true
+            :length, :skill_level, :skill_list, :youtube_id,
+            presence: true
 
   default_scope -> {
     order("no desc")
