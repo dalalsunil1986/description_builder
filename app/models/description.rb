7d0bbfecbd62840e1db79b69766bd779490b6584
Mark Tellez <mark.tellez@icloud.com>
2018-05-06 22:57:29 -0500

    default ordering by no

--- app/models/description.rb
+++ app/models/description.rb
@@ -1,4 +1,8 @@
 class Description < ApplicationRecord
   validates :no, :title, :description, :repo_url,
             :length, :skill_level, :skill_list, presence: true
+
+  default_scope -> {
+    order("no desc")
+  }
 end
