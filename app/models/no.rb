d23182b8a158fdcb97a24b367b68b43732a015a8
Mark Tellez <mark.tellez@icloud.com>
2018-05-06 22:53:51 -0500

    no and name on show

--- app/models/no.rb
+++
@@ -1,2 +0,0 @@
-class No < ApplicationRecord
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180507024050) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "descriptions", force: :cascade do |t|
     t.string "no"
     t.string "title"
